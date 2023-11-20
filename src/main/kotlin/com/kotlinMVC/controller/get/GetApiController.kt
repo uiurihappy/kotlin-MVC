9b863a058a082b607febb7f8cba93c83cc86ae10
ybchar <uiurihappy@sju.ac.kr>
2023-11-20 18:00:50 +0900

    Feat: Http Method GET

--- src/main/kotlin/com/kotlinMVC/controller/get/GetApiController.kt
+++ src/main/kotlin/com/kotlinMVC/controller/get/GetApiController.kt
@@ -1,7 +1,11 @@
 package com.kotlinMVC.controller.get
 
+import com.kotlinMVC.model.http.UserRequest
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController     // REST API Controller
@@ -11,5 +15,48 @@ class GetApiController {
     @GetMapping("/hello")
     fun hello(): String {
         return "hello kotlin";
+    }
+
+    // HTTP 메소드와 별개로 호출 메소드에 종속된다.
+    @RequestMapping(method = [RequestMethod.GET], path = ["request-mapping"])
+    fun requestMapping(): String {
+        return "request-mapping";
+    }
+
+    @GetMapping("/get-mapping/path-variable/{name}/{age}") // GET http://localhost:8080/api/get-mapping/path-variable/steve/27
+    fun pathVariable(@PathVariable name: String, @PathVariable age:Int): String {
+        println("${name}, ${age}")
+        return "pathVar $name $age"
+    }
+
+    @GetMapping("/get-mapping/path-variable2/{name}/{age}") // GET http://localhost:8080/api/get-mapping/path-variable2/steve/27
+    fun pathVariable2(@PathVariable(value = "name") _name: String, @PathVariable(name = "age") age:Int): String {
+        val name = "kotlin"
+
+        println("${_name}, $age")
+        return "pathVar2 $_name $age"
+    }
+
+    @GetMapping("/get-mapping/query-param")     // GET http://localhost:8080/api/get-mapping/query-param?name=steve&age=27
+    fun queryParam(@RequestParam(name = "name") name: String, @RequestParam(value = "age") age: Int): String {
+        println("$name, $age")
+        return "query $name $age"
+    }
+
+    // name, age, address, email
+    @GetMapping("/get-mapping/query-param/object") // GET http://localhost:8080/api/get-mapping/query-param/object
+    // http://localhost:8080/api/get-mapping/query-param/object?name=steve&age=10&email=uiurihappy@gmail.com&address=seoul
+    fun queryParamObject(userRequest: UserRequest): UserRequest {
+        println(userRequest)
+        return userRequest
+    }
+
+    @GetMapping("/get-mapping/query-param/map") // GET http://localhost:8080/api/get-mapping/query-param/map
+    // http://localhost:8080/api/get-mapping/query-param/map?name=steve&age=10&email=uiurihappy@gmail.com&address=seoul
+    fun queryParamMap(@RequestParam map: Map<String, Any>): Map<String, Any> {
+        println(map)
+        val phoneNumber = map.get("phone-number")
+        println(phoneNumber)
+        return map
     }
 }
---
+++ src/main/kotlin/com/kotlinMVC/model/http/UserRequest.kt
@@ -0,0 +1,9 @@
+package com.kotlinMVC.model.http
+
+data class UserRequest(
+    var name:String?=null,
+    var age:Int?=null,
+    var email:String?=null,
+    var address:String?=null,
+    var phoneNumber:String?=null,
+)
