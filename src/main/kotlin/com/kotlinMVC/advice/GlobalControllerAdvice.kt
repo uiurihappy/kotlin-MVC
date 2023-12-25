f92acdf6e12bc4fa07526d7e5c6ea0900c9132bb
ybchar <uiurihappy@sju.ac.kr>
2023-12-25 18:34:34 +0900

    Feat: ConstraintViolationException 에러 처리

--- src/main/kotlin/com/kotlinMVC/advice/GlobalControllerAdvice.kt
+++ src/main/kotlin/com/kotlinMVC/advice/GlobalControllerAdvice.kt
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
-@RestControllerAdvice(basePackageClasses = [ExceptionApiController::class])
+//@RestControllerAdvice
 class GlobalControllerAdvice {
 
     @ExceptionHandler(value = [RuntimeException::class])
