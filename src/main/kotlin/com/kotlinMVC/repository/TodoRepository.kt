731871aa209805770122b2bc387339475f890ed7
ybchar <uiurihappy@sju.ac.kr>
2024-01-01 11:51:01 +0900

    Feat: In-memory DB 설계 및 테스트

--- src/main/kotlin/com/kotlinMVC/repository/TodoRepository.kt
+++ src/main/kotlin/com/kotlinMVC/repository/TodoRepository.kt
@@ -3,7 +3,7 @@ package com.kotlinMVC.repository
 import com.kotlinMVC.database.Todo
 
 interface TodoRepository {
-    fun save(todo: Todo): Todo
+    fun save(todo: Todo): Todo?
     fun saveAll(todoList: MutableList<Todo>): Boolean
 
     fun delete(index: Int): Boolean
@@ -11,7 +11,5 @@ interface TodoRepository {
     fun findOne(index: Int): Todo?
 
     fun findAll(): MutableList<Todo>
-
-    fun update(index: Int, todo: Todo): Todo?
 
 }
--- src/test/kotlin/com/kotlinMVC/repository/TodoRepositoryTest.kt
+++ src/test/kotlin/com/kotlinMVC/repository/TodoRepositoryTest.kt
@@ -2,16 +2,26 @@ package com.kotlinMVC.repository
 
 import com.kotlinMVC.config.AppConfig
 import com.kotlinMVC.database.Todo
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit.jupiter.SpringExtension
 import java.time.LocalDateTime
 
+@ExtendWith(SpringExtension::class) // Spring의 확장 기능을 사용
 @SpringBootTest(classes = [TodoRepositoryImpl::class, AppConfig::class])
 class TodoRepositoryTest {
 
     @Autowired
     lateinit var todoRepositoryImpl: TodoRepositoryImpl
+
+    @BeforeEach
+    fun setUp() {
+        todoRepositoryImpl.todoDatabase.init() // 초기화 함수 호출
+    }
 
     @Test
     fun saveTest() {
@@ -20,6 +30,72 @@ class TodoRepositoryTest {
             this.description = "테스트"
             this.schedule = LocalDateTime.now()
         }
-        println(todo)
+
+        val result = todoRepositoryImpl.save(todo)
+        Assertions.assertEquals(1, result?.index)
+        Assertions.assertNotNull(result?.createdAt)
+        Assertions.assertNotNull(result?.updatedAt)
+        Assertions.assertEquals("테스트 일정", result?.title)
+        Assertions.assertEquals("테스트", result?.description)
+    }
+
+    @Test
+    fun  saveAllTest() {
+        val todoList = mutableListOf<Todo>().apply {
+            for (i in 1..10) {
+                add(Todo().apply {
+                    this.title = "테스트 일정 $i"
+                    this.description = "테스트"
+                    this.schedule = LocalDateTime.now()
+                })
+            }
+        }
+
+        val result = todoRepositoryImpl.saveAll(todoList)
+        Assertions.assertTrue(result)
+    }
+
+    @Test
+    fun findOneTest() {
+
+        val todoList = mutableListOf<Todo>().apply {
+            for (i in 1..10) {
+                add(Todo().apply {
+                    this.title = "테스트 일정 $i"
+                    this.description = "테스트 $i"
+                    this.schedule = LocalDateTime.now()
+                })
+            }
+        }
+
+        todoRepositoryImpl.saveAll(todoList)
+        val result = todoRepositoryImpl.findOne(2)
+
+        Assertions.assertNotNull(result)
+        Assertions.assertEquals("테스트 일정 2", result?.title)
+        Assertions.assertEquals("테스트 2", result?.description)
+    }
+
+    @Test
+    fun updateTest() {
+        val todo = Todo().apply {
+            this.title = "테스트 일정"
+            this.description = "테스트"
+            this.schedule = LocalDateTime.now()
+        }
+        val insertTodo = todoRepositoryImpl.save(todo)
+
+        val newTodo = Todo().apply {
+            this.index = insertTodo?.index
+            this.title = "업데이트 일정"
+            this.description = "업데이트 테스트"
+            this.schedule = LocalDateTime.now()
+        }
+        val result = todoRepositoryImpl.save(newTodo)
+
+        Assertions.assertNotNull(result)
+        Assertions.assertEquals(insertTodo?.index, result?.index)
+        Assertions.assertEquals("업데이트 일정", result?.title)
+        Assertions.assertEquals("업데이트 테스트", result?.description)
     }
 }
