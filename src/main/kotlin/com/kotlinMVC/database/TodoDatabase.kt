247956173886eb0de4371295b9569ae4fd2ab433
ybchar <uiurihappy@sju.ac.kr>
2024-01-01 11:51:01 +0900

    Feat: In-memory DB 설계 및 테스트

--- src/main/kotlin/com/kotlinMVC/database/TodoDatabase.kt
+++ src/main/kotlin/com/kotlinMVC/database/TodoDatabase.kt
@@ -5,6 +5,7 @@ data class TodoDatabase (
     var todoList: MutableList<Todo> = mutableListOf()
 ) {
     fun init() {
+        this.index = 0  // init 시 index 초기화
         this.todoList = mutableListOf()
         println("[DEBUG] todo database init")
     }
--- src/main/kotlin/com/kotlinMVC/repository/TodoRepositoryImpl.kt
+++ src/main/kotlin/com/kotlinMVC/repository/TodoRepositoryImpl.kt
@@ -4,6 +4,7 @@ import com.kotlinMVC.database.Todo
 import com.kotlinMVC.database.TodoDatabase
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import java.time.LocalDateTime
 
 @Service
 class TodoRepositoryImpl: TodoRepository{
@@ -11,33 +12,66 @@ class TodoRepositoryImpl: TodoRepository{
     @Autowired
     lateinit var todoDatabase: TodoDatabase
 
-    override fun save(todo: Todo): Todo {
-        return todo.apply {
-            this.index = todoDatabase.index++
-        }.run {
-            todoDatabase.todoList.add(todo)
-            this
+    override fun save(todo: Todo): Todo? {
+
+        // 1. index 검사
+        // 2. index 값이 있으면 업데이트
+        // 3. index 값이 없으면 생성
+        return todo.index?.let {index ->
+            // 값이 있을 때 update
+            findOne(index)?.apply {
+                this.title = todo.title
+                this.description = todo.description
+                this.schedule = todo.schedule
+                this.updatedAt = LocalDateTime.now()
+            }
+        }?: kotlin.run {
+            // 값이 없을 때 insert
+            todo.apply {
+                this.index = ++todoDatabase.index
+                this.createdAt = LocalDateTime.now()
+                this.updatedAt = LocalDateTime.now()
+            }.run {
+                todoDatabase.todoList.add(todo)
+                this
+            }
         }
     }
 
     override fun saveAll(todoList: MutableList<Todo>): Boolean {
-        TODO("Not yet implemented")
+        return try {
+            todoList.forEach {
+                save(it)
+            }
+            true
+        } catch (e: Exception) {
+            false
+        }
     }
 
     override fun delete(index: Int): Boolean {
-        TODO("Not yet implemented")
+        return findOne(index)?.let {
+            // 값이 있을 때
+            todoDatabase.todoList.remove(it)
+            true
+        }?: kotlin.run {
+            // 값이 없을 때
+            false
+        }
+//        return todoDatabase.todoList.removeIf {
+//            it.index == index
+//        }
     }
 
+    // 없을 수도 있기에 반환타입으로 null도 받을 수 있도록 ? 추가
     override fun findOne(index: Int): Todo? {
-        TODO("Not yet implemented")
+        // 동일한 인덱스의 데이터 찾으며 첫번째 데이터 반환
+        return todoDatabase.todoList.first {
+            it.index == index
+        }
     }
 
     override fun findAll(): MutableList<Todo> {
-        TODO("Not yet implemented")
+        return todoDatabase.todoList
     }
-
-    override fun update(index: Int, todo: Todo): Todo? {
-        TODO("Not yet implemented")
-    }
-
 }
